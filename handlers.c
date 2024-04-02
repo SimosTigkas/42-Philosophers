2975d000649f68f633011543397e30b9c0a3d1e9
Simos Tigkas <stigkas@c1r6p16.hive.fi>
2024-04-02 16:55:47 +0300

    Almost finnished

--- handlers.c
+++ handlers.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:42:48 by stigkas           #+#    #+#             */
-/*   Updated: 2024/04/02 15:34:06 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/04/02 16:53:45 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int	mtx_error(int mtx_err, t_mtx_action act)
 {
-	if (!mtx_err && act == INIT)
+	if (mtx_err != 0 && act == INIT)
 	{
 		printf("The values specified by attr are not valid\n");
 		return (0);
 	}
-	else if (!mtx_err && act == DESTROY)
+	else if (mtx_err != 0 && act == DESTROY)
 	{
 		printf("Mutex could not be destroyed\n");
 		return (0);
 	}
-	else if (!mtx_err && act == LOCK)
+	else if (mtx_err != 0 && act == LOCK)
 	{
 		printf("Mutex could not be locked\n");
 		return (0);
 	}
-	else if (!mtx_err && act == UNLOCK)
+	else if (mtx_err != 0 && act == UNLOCK)
 	{
 		printf("Mutex could not be unlocked\n");
 		return (0);
@@ -66,17 +66,17 @@ int	mtx_handler(pthread_mutex_t *mtx, t_mtx_action act)
 
 int	th_error(int thread_err, t_thread_action act)
 {
-	if (!thread_err && act == CREATE)
+	if (thread_err != 0 && act == CREATE)
 	{
 		printf("Thread could not be created\n");
 		return (0);
 	}
-	else if (!thread_err && act == JOIN)
+	else if (thread_err != 0 && act == JOIN)
 	{
 		printf("Thread could not be joined\n");
 		return (0);
 	}
-	else if (!thread_err && act == DETACH)
+	else if (thread_err != 0 && act == DETACH)
 	{
 		printf("Thread could not be detached\n");
 		return (0);
